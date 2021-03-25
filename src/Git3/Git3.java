8c8e4fecc39059d9872ac2618febc174d85e178e
jmgh85 <jmgh85@gmail.com>
2021-03-25 13:18:23 +0100

    xx

---
+++ src/Git3/Git3.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Git3;
+
+/**
+ *
+ * @author jguther0
+ */
+public class Git3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         String exampleString = "This is just a sample string2";
+
+        int stringLength = exampleString.length();
+
+        System.out.println("String length: " + stringLength);
+
+        int stringLengthWithoutSpaces = exampleString.replace(" ", "").length();
+        System.out.println("String length without counting whitespaces: " + stringLengthWithoutSpaces);
+    }
+    
+}
